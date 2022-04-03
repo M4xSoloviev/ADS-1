e5765376ec65cc0acc4c04f19bad80948ec45eda
M4xSoloviev <79105782+M4xSoloviev@users.noreply.github.com>
2022-04-03 19:52:23 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,7 +5,7 @@
 
 bool checkPrime(uint64_t value) {
 // вставьте код функции
- for (int i = 2; i < value; i++) {
+  for (int i = 2; i < value; i++) {
        if (value % i == 0) {
          return false;
             break;
@@ -16,7 +16,7 @@ bool checkPrime(uint64_t value) {
 
 uint64_t nPrime(uint64_t n) {
 // вставьте код функции
-    int count = 0;
+  int count = 0;
     int value = 1;
     while (count != n) {
         value++;
@@ -29,7 +29,7 @@ uint64_t nPrime(uint64_t n) {
 
 uint64_t nextPrime(uint64_t value) {
 // вставьте код функции
-    int v = value+1;
+  int v = value+1;
     while (true) {
     if (checkPrime(v))
       return v;
@@ -40,7 +40,7 @@ uint64_t nextPrime(uint64_t value) {
 
 uint64_t sumPrime(uint64_t hbound) {
 // вставьте код функции
-    uint64_t summ = 0;
+  uint64_t summ = 0;
     while (hbound !=2) {
         hbound--;
         if (checkPrime(hbound) == true) {
